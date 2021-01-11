8cb4ababfe98d7c9ba1bb8f51c224297baca5110
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-01-11 21:21:55 +0530

    Create MinMaxSum.cpp

---
+++ Practice/Algorithms/Warmup/MinMaxSum.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+void miniMaxSum(vector<int> arr) {
+    int size = arr.size();
+    vector<long long> sum(2);
+    
+    sort(arr.begin(), arr.end());
+    
+    for (int i = 0; i < size - 1; i++)
+        sum[0] += arr[i];
+
+    for (int i = 1; i < size; i++)
+        sum[1] += arr[i];
+
+    cout << sum[0] << " " << sum[1];
+}
+
+int main()
+{
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
