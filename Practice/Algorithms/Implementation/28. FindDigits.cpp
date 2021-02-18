a5afdc2514d94140b23b2bba76e8c3a3de20a009
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-02-19 00:52:50 +0530

    Create 28. FindDigits.cpp

---
+++ Practice/Algorithms/Implementation/28. FindDigits.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDigits(int n) {
+    int m = n, count = 0;
+    vector<int> numbers;
+    while (n > 0) {
+        numbers.push_back(n % 10);
+        n /= 10;
+    }
+    for (int i: numbers)
+        if (i != 0)
+            if (m % i == 0)
+                count++;
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = findDigits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
