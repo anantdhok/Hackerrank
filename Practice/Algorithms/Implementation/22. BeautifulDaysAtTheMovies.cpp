6a63f1d56c28bbb96052865e7e70d073b3876a3d
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-02-18 13:54:40 +0530

    Create 22. BeautifulDaysAtTheMovies.cpp

---
+++ Practice/Algorithms/Implementation/22. BeautifulDaysAtTheMovies.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+
+int reverseNumber(int n) {
+    int r = 0;
+    while (n > 0) {
+        r = r * 10 + (n % 10);
+        n /= 10;
+    }
+    return r;
+}
+
+int beautifulDays(int i, int j, int k) {
+    int days = 0;
+    for (int d = i; d <= j; d++) {
+        float n = abs(d - reverseNumber(d)) / (float)k;
+        if (floor(n) == n)
+            days++;
+    }
+    return days;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string ijk_temp;
+    getline(cin, ijk_temp);
+
+    vector<string> ijk = split_string(ijk_temp);
+
+    int i = stoi(ijk[0]);
+
+    int j = stoi(ijk[1]);
+
+    int k = stoi(ijk[2]);
+
+    int result = beautifulDays(i, j, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
