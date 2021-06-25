70a4f69c0676fad0c56485e5bcaf2b2d9c6f6e54
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-06-25 22:45:49 +0530

    Create Day 0: Begining.js

--- 10 Days of JavaScript/Day 0: Begining.js
+++ 10 Days of JavaScript/Day 0: Begining.js
@@ -22,12 +22,10 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
 function greeting(parameterVariable) {
     console.log('Hello, World!');
     console.log(parameterVariable);
 }
-
 
 function main() {
     const parameterVariable = readLine();
