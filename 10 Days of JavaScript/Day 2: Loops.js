80e2ec5617f90c805ac3bbd03371afe959310867
Anant Dhok <67429427+anantdhok@users.noreply.github.com>
2021-06-25 22:25:21 +0530

    Create Day 2: Loops.js

---
+++ 10 Days of JavaScript/Day 2: Loops.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function vowelsAndConsonants(s) {
+    let vowels = ["a", "e", "i", "o", "u"];
+
+    for(var i of s)
+        if(vowels.includes(i))
+            console.log(i);
+    
+    for(var i of s)
+        if(!vowels.includes(i))
+            console.log(i);
+}
+
